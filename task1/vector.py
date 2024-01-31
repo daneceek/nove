bb63ca1652087efac352796a3e89ea80fc467aa8
Danny Kročil <dannykrocil@seznam.cz>
2024-01-31 14:17:23 +0100

    init

---
+++ task1/vector.py
@@ -0,0 +1,26 @@
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __repr__(self):
+        return f"Vector({self.x}, {self.y})"
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y 
+    def __add__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x + other.x, self.y + other.y)
+        else:
+            raise ValueError("Nefunguje správně")
+    def __sub__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x - other.x, self.y - other.y)
+        else:
+            raise ValueError("Nefunguje správně")
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            return Vector(self.x * other, self.y * other)
+        else:
+            raise ValueError("Nefunguje správně")
+    
+    

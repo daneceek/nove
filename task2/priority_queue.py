e5d007b7cbb9d7278311544d74be32be305343f7
Danny Kročil <dannykrocil@seznam.cz>
2024-02-07 14:12:24 +0100

    update

--- task2/priority_queue.py
+++ task2/priority_queue.py
@@ -1,8 +1,20 @@
 class PriorityQueue:
-    pass
+    def __init__(self):
+        self.queue = []
+        self.priority = []
+    def push(self, item, priority):
+        self.queue.append(item)
+        self.priority.append(priority)
+    
+    
+    
+    def __len__(self):
+        return len(self.queue)
 
-
-
+    
+    def __iter__(self):
+        return iter(self.queue)
+    
 priority_queue = PriorityQueue()
 priority_queue.push("Task 1", 3)
 priority_queue.push("Task 2", 1)
@@ -18,3 +30,5 @@ print("Processing tasks in Priority Order:")
 while len(priority_queue) > 0:
     task = priority_queue.pop()
     print("Processing:", task)
+
+# nejmensi cislo ma nejmensi prioritu

ed8e97d56e62b5d7ab00550fd159d58f27da83d7
Danny Kročil <dannykrocil@seznam.cz>
2024-02-13 21:14:46 +0100

    updat

--- task2/priority_queue.py
+++ task2/priority_queue.py
@@ -1,34 +1,55 @@
+
+
 class PriorityQueue:
     def __init__(self):
         self.queue = []
-        self.priority = []
+        self.priority_array = []
+        self.completed_sort = 0
     def push(self, item, priority):
-        self.queue.append(item)
-        self.priority.append(priority)
-    
-    
-    
+        dictionary = {}
+        dictionary[item] = priority
+        self.queue.append(dictionary)
+        self.priority_array.append(priority)
+        
     def __len__(self):
         return len(self.queue)
 
+    def pop(self):
+        if len(self.queue) == 0:
+            raise IndexError
+        
+        for _ in range(1, len(self.queue)):
+            for item in self.queue:
+                for task, priority in item.items():
+                    if priority == min(self.priority_array):
+                        self.queue.remove(item)
+                        return task
     
     def __iter__(self):
-        return iter(self.queue)
+        self.position = 0
+        return self
+    def __next__(self):
+        print(self.position)
+        print(self.queue)
+        print(len(self.queue))
+        if self.position < len(self.queue):
+            item = self.queue[self.position]
+            for item, priority in item.items():
+                if priority == min(self.priority_array):
+                    return item
+                else:
+                    self.position +=1
+        
+        
+
     
-priority_queue = PriorityQueue()
-priority_queue.push("Task 1", 3)
-priority_queue.push("Task 2", 1)
-priority_queue.push("Task 3", 2)
+        
+pq = PriorityQueue()
+pq.push("Task 1", 3)
+pq.push("Task 2", 1)
+pq.push("Task 3", 2)
 
-print("Priority Queue Length:", len(priority_queue))
-
-print("Tasks in Priority Order:")
-for task in priority_queue:
-    print(task)
-
-print("Processing tasks in Priority Order:")
-while len(priority_queue) > 0:
-    task = priority_queue.pop()
-    print("Processing:", task)
+tasks = list(pq)
+print(tasks)
 
 # nejmensi cislo ma nejmensi prioritu
